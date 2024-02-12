3f11b7d95844601afa1daa451953a0eab54eb207
helcio <h.andre@giap.pl>
2024-02-12 16:11:26 +0100

    Phase 01 Connecting HTML With Javascript completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,20 @@
-changeTitle();
-changeHeader();
-changeAboutMe();
-console.log(findElementById("header")); // <div id="header">
-console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+import { changeAboutMe, changeHeader, changeTitle } from "./manipulate.js";
+import {
+  findElementById,
+  findElementsOfClass,
+  findFirstElementOfTag,
+  findFirstElementOfClass,
+  findElementsOfTag,
+} from "./search.js";
+
+window.onload = () => {
+  alert("document ready!");
+  changeTitle();
+  changeHeader();
+  changeAboutMe();
+  console.log(findElementById("header")); // <div id="header">
+  console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
+  console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
+  console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+  console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+};
--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,15 @@
-function changeTitle() {
-    // Change the title of the page to "(Your name)'s Portfolio"
-
-    // Your code here
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
+  // Your code here
 }
 
-function changeHeader() {
-    // Change the name in the h1 of the page to your name
-
-    // Your code here
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
+  // Your code here
 }
 
-function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
+export function changeAboutMe() {
+  /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
-    // Your code here
+  // Your code here
 }
