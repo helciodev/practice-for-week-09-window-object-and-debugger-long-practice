5348676608873505ce98f15c44b0c3034fb85ff8
helcio <h.andre@giap.pl>
2024-02-12 16:20:23 +0100

    Complete phase 03 Filling out the Modules

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,15 +1,17 @@
 export function changeTitle() {
   // Change the title of the page to "(Your name)'s Portfolio"
-  // Your code here
+  document.title = `Helcio's Portfolio`;
 }
 
 export function changeHeader() {
   // Change the name in the h1 of the page to your name
   // Your code here
+  document.body.children[0].children[0].innerText = "Helcio André";
 }
 
 export function changeAboutMe() {
   /* Update the first paragraph in the About Me section with a small
      passage about yourself */
   // Your code here
+  document.body.children[1].children[1].innerText = "I am a gamer by nature";
 }
